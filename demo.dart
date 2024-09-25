25ebc8532dd1b06dba0e297f2d076eba72956bd6
rahulmodhave099 <rahulmodhave099@gmail.com>
2024-09-25 23:59:27 +0530

    demo

---
+++ demo.dart
@@ -0,0 +1,16 @@
+
+void main()
+{
+  // This is my first code
+  print("Hello World");             // Another comment
+  print("This is my First Application");
+  // Printing numerical value
+  print(202);
+  /*
+  Multiple lines comment.
+  printing boolean value 
+  */
+  print(true);
+  print(4 * 2);
+
+}
